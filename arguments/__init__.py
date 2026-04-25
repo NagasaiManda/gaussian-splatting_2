8e35362bd1cce1be7d98bf78373496fb20290b69
NagasaiManda <138184417+NagasaiManda@users.noreply.github.com>
2026-04-25 21:15:57 +0530

    Updated parameters

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -92,8 +92,12 @@ class OptimizationParams(ParamGroup):
         # self.opacity_reset_interval = 3000
         self.opacity_reset_interval = 2000        # For aeroplane
         self.densify_from_iter = 500
-        self.densify_until_iter = 15_000
-        self.densify_grad_threshold = 0.0002
+        # self.densify_until_iter = 15_000
+        self.densify_until_iter = 10_000
+        
+        # self.densify_grad_threshold = 0.0002
+        self.densify_grad_threshold = 0.0004
+        
         self.depth_l1_weight_init = 1.0
         self.depth_l1_weight_final = 0.01
         self.random_background = False
